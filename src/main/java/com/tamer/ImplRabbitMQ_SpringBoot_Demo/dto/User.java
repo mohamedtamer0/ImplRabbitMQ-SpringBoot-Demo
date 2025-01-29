adc0476214f96988af19eea854efebe4038a3edc
mohamedtamer0 <mt4452242@gmail.com>
2025-01-29 21:03:00 +0200

    Configure RabbitMQ for JSON Message Communication

---
+++ src/main/java/com/tamer/ImplRabbitMQ_SpringBoot_Demo/dto/User.java
@@ -0,0 +1,10 @@
+package com.tamer.ImplRabbitMQ_SpringBoot_Demo.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
